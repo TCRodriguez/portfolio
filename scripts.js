13c201226ee39e642c56baf1f86830a986e65a81
TCRodriguez <tonatiuh.cuauhtemoc@gmail.com>
2020-04-24 15:18:18 -0700

    addd 'hyperlink' function via jQuery to 'projects' section

---
+++ scripts.js
@@ -0,0 +1,26 @@
+
+
+
+    // =====================================
+// jQuery for 'projects' section 
+// =====================================
+
+$("#landing-page-project").click(function() {
+    window.open("https://codepen.io/TRodriguez/full/jObbWmR", '_blank');
+});
+
+$("#portfolio-project").click(function() {
+    window.open("https://codepen.io/TRodriguez/full/mderzeR", '_blank');
+});
+
+$("#tribute-project").click(function() {
+    window.open("https://codepen.io/TRodriguez/full/abOKrNN", '_blank');
+});
+
+$("#survey-form-project").click(function() {
+    window.open("https://codepen.io/TRodriguez/full/NWqomeQ", '_blank');
+});
+
+$("#construction-site-redesign").click(function() {
+    window.open("https://codepen.io/TRodriguez/full/bGdVJMR", '_blank');
+});
