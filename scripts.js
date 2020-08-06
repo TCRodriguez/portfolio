4a96261c9d799216a07c334ba6b5713c3701b3a9
TCRodriguez <tonatiuh.cuauhtemoc@gmail.com>
2020-08-06 14:31:53 -0700

    add new card for palindrome-checker; comment out jQuery due to button override issue; update stylesheet to make room for new project tiles up to 9 total

--- scripts.js
+++ scripts.js
@@ -4,27 +4,31 @@
     // =====================================
 // jQuery for 'projects' section 
 // =====================================
-$("#technical-documentation-page").click(function() {
-    window.open("https://codepen.io/TRodriguez/full/oNjyqJY", '_blank');
-});
+// $("#technical-documentation-page").click(function() {
+//     window.open("https://codepen.io/TRodriguez/full/oNjyqJY", '_blank');
+// });
 
 
-$("#landing-page-project").click(function() {
-    window.open("https://codepen.io/TRodriguez/full/jObbWmR", '_blank');
-});
+// $("#landing-page-project").click(function() {
+//     window.open("https://codepen.io/TRodriguez/full/jObbWmR", '_blank');
+// });
 
-$("#portfolio-project").click(function() {
-    window.open("https://codepen.io/TRodriguez/full/mderzeR", '_blank');
-});
+// $("#palindrome-checker").click(function() {
+//     window.open("https://codepen.io/TRodriguez/full/OJNPXdx", '_blank');
+// });
 
-$("#tribute-project").click(function() {
-    window.open("https://codepen.io/TRodriguez/full/abOKrNN", '_blank');
-});
+// $("#portfolio-project").click(function() {
+//     window.open("https://codepen.io/TRodriguez/full/mderzeR", '_blank');
+// });
 
-$("#survey-form-project").click(function() {
-    window.open("https://codepen.io/TRodriguez/full/NWqomeQ", '_blank');
-});
+// $("#tribute-project").click(function() {
+//     window.open("https://codepen.io/TRodriguez/full/abOKrNN", '_blank');
+// });
 
-$("#construction-site-redesign").click(function() {
-    window.open("https://codepen.io/TRodriguez/full/bGdVJMR", '_blank');
-});
+// $("#survey-form-project").click(function() {
+//     window.open("https://codepen.io/TRodriguez/full/NWqomeQ", '_blank');
+// });
+
+// $("#construction-site-redesign").click(function() {
+//     window.open("https://codepen.io/TRodriguez/full/bGdVJMR", '_blank');
+// });
